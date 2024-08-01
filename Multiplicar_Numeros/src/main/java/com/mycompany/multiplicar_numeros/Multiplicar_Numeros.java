ca25c0b9509a5a26dd7ad6d11887fc1dfafcb5c3
St <nickname@gmail.com>
2024-08-01 10:25:07 -0500

    Se agrega la clase de multiplicar numeros.java

---
+++ Multiplicar_Numeros/src/main/java/com/mycompany/multiplicar_numeros/Multiplicar_Numeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multiplicar_numeros;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Multiplicar_Numeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
