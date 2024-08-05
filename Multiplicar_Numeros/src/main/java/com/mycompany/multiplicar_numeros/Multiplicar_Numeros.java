6768abecc3ecf13dd64610a80ff22f3f2a1465f4
sinnombre <noaplica@gmail.com>
2024-08-05 10:15:31 -0500

    Se sube el programa para multiplicar

--- Multiplicar_Numeros/src/main/java/com/mycompany/multiplicar_numeros/Multiplicar_Numeros.java
+++ Multiplicar_Numeros/src/main/java/com/mycompany/multiplicar_numeros/Multiplicar_Numeros.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.multiplicar_numeros;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,10 @@ package com.mycompany.multiplicar_numeros;
 public class Multiplicar_Numeros {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner m = new Scanner(System.in);
+        double numero1 = m.nextDouble();
+        double numero2 = m.nextDouble();
+        double resultado = numero1 * numero2;
+        System.out.println("El resultado de la multiplicación es: " + resultado);
     }
 }
